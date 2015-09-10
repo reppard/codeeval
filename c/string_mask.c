d7a5fcf13320b5f6a1d60b2826b4941f753123a0
reppard <reppardwalker@gmail.com>
2015-09-09 20:58:13 -0400

    feat(string_mask.c): refactor

--- c/string_mask.c
+++ c/string_mask.c
@@ -6,28 +6,14 @@ int main(int argc, char *argv[])
   FILE *file = argv[1] ? fopen(argv[1], "r") : fopen("string_mask.txt", "r");
   char line[1024];
 
-  while (fgets(line, 1024, file)) {
-    int line_length = strlen(line);
-    int word_length = (line_length / 2);
-    char word[word_length];
-    char cipher[word_length];
-
-    int i;
-
-    for(i = 0; i < word_length; i++) {
-      word[i] = line[i];
-      cipher[i] = line[i+word_length];
-    }
-
-    word[word_length-1] ='\0';
-    cipher[word_length-1] = '\0';
-
-    for(i = 0; i < word_length - 1 ; i++) {
-      if(cipher[i] == '1') {
-        word[i] -= 32;
+  while (fgets(line, 1024, file) != NULL) {
+    char *lhs = line, *rhs = strchr(line,' ');
+    for(*rhs++ = '\0'; *lhs && *rhs; ++lhs, ++rhs){
+      if(*rhs == '1') {
+        *lhs -= 32;
       }
     }
-    printf("%s\n", word);
+    puts(line);
   }
   return 0;
 }
